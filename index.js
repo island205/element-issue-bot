501ce3e337beb24506c4e8334bc9367fd2eabb90
Zhi Cun <island205@gmail.com>
2018-11-05 17:13:01 +0800

    add enableAssign config

--- index.js
+++ index.js
@@ -7,7 +7,13 @@ const { addAssignees, translate, referToFaq, notifyEmptyFiddle } = require('./se
 const util = require('./utils')
 const github = require('./github')
 
+const config = {
+  enableAssign: false
+}
+
 const app = new Koa()
+
+
 
 app.use(logger())
 app.use(bodyParser)
@@ -19,8 +25,8 @@ app.use(async (ctx, next) => {
     return
   }
 
-  if (action === 'labeled') {
-    // await addAssignees(ctx, issue)
+  if (config.enableAssign && action === 'labeled') {
+    await addAssignees(ctx, issue)
     return
   }
 
